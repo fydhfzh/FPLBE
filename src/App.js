a5cfa30c85820076b381476e2bc97cbeed2d67c0
Fayyadh Hafizh <you@example.com>
2021-09-23 16:26:20 +0700

    hasil perbaikan darkmode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import './App.css';
 import './index.css';
 import Navbar from './parent_components/Navbar';
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
 
 
 function App() {
   return (
     <ChakraProvider>
-        <Navbar />
+        <Navbar initialColorMode='light'/>
     </ChakraProvider>
       
   )
