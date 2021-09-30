073306d65ca12feb48f233f4783c9d2a27cb826f
Fayyadh Hafizh <you@example.com>
2021-09-30 12:35:13 +0700

    fixing footer

--- src/parent_components/Footer.js
+++ src/parent_components/Footer.js
@@ -7,7 +7,7 @@ import { GrFacebookOption, GrInstagram, GrLinkedin } from 'react-icons/gr'
 
 function Footer() {
     return (
-        <Box  height={300} bg="transparent" >
+        <Box  height={300} bg="transparent" mt='20%'>
             <Center>
                 <Box height={300} width={1000} borderTopWidth={1} borderTopColor="#d3d6bf">
                     <Box marginLeft={100} marginTop={50} width={300}>
